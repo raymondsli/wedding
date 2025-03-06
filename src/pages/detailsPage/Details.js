4ec05d6316f8d1a13f687014fc8b463819fff7a9
Raymond Li <raymond.s.li@gmail.com>
2025-03-05 19:48:06 -0800

    Add countdown clock

--- src/pages/detailsPage/Details.js
+++ src/pages/detailsPage/Details.js
@@ -1,11 +1,16 @@
 import "./Details.css";
 import DetailsContent from "./DetailsContent";
+import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
+import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 
 function Details() {
   return (
     <div className="sky-background-image">
       <h1 className="name-header">Yi-Nung & Raymond</h1>
       <DetailsContent />
+      <div className="countdown-clock">
+        <FlipClockCountdown to={"2025-09-06T17:00:00"} />
+      </div>
     </div>
   );
 }
--- src/pages/detailsPage/DetailsContent.js
+++ src/pages/detailsPage/DetailsContent.js
@@ -4,8 +4,7 @@ function DetailsContent() {
   return (
     <div className="details-content">
       <div class="details-text">
-        <div>6th September 2025</div>
-        <div>Pines Park</div>
+        <div>6th September 2025 at Pines Park</div>
         <div>34941 Camino Capistrano, Capistrano Beach, CA 92624</div>
       </div>
     </div>
