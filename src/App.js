cfb33e027a2a3e2335e6bd41c1d1854a6656a562
Raymond Li <rli@Yis-MacBook-Air.local>
2025-03-05 16:46:45 -0800

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello World! Yi-Nung + Raymond
+      </header>
+    </div>
+  );
+}
+
+export default App;
