dba5d2bb60590c6c3b88ce01548a9a1cf2e25a4b
Raymond Li <raymond.s.li@gmail.com>
2025-03-07 15:44:38 -0800

    Update faq

--- src/pages/faqPage/FAQ.js
+++ src/pages/faqPage/FAQ.js
@@ -12,13 +12,13 @@ const questionAndAnswers = {
   "Will the venue be outdoors or indoors?":
     "The ceremony will be at Pines Park, which is an outdoor public park. For dinner, we have booked a private indoor room at Whitestone Restaurant, which is about a 10 minute drive away from the park venue.",
   "Will there be parking at the ceremony venue?":
-    "Yes, there is limited street parking at the park, but it is located in a residential area with street parking.",
+    "Yes, there is limited street parking at the park, and it is located in a residential area with street parking.",
   "Will there be a restroom at the ceremony venue?":
     "Unfortunately, there is no public restroom at Pines Park. We will try to keep the ceremony short, but if you do need to go at any time feel free to just leave the venue.",
   "What time should I arrive at the restaurant?":
     "We have booked the private room at Whitestone from 4:30pm - 10:00pm. We expect the ceremony to end at around 6:00pm, after which you are free to go directly to the restaurant. We will start serving food at 7:15pm so you can arrive anytime before then.",
   "What do I do when I arrive at the restaurant?":
-    "When you arrive, either go directly to the private room or ask restaurant staff to take you there. Note that if you are early, the room may be empty when you arrive. We will make seating arrangements for each guest, so in the room you can look for your name and find your seat. If the seating arrangements have not been set up yet, please wait until we finish setting up.",
+    "When you arrive, either go directly to the private room or ask restaurant staff to take you there. Note that if you are early, the room may be empty when you arrive. We will make seating arrangements for each guest, so once in the room you can look for your name and find your seat. If the seating arrangements have not been set up yet, please wait until we finish setting up.",
   "Will there be parking at the restaurant?":
     "Yes there is parking at the restaurant. If the parking at the restaurant is full, you can park at nearby residential street parking.",
   "Will there be drinks at the restaurant?":
